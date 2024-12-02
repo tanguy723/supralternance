0e3583115e7c2fc2741cd7c2ed9a87cfcbd58bbf
unknown <tanguyvh1@gmail.com>
2024-12-02 15:53:53 +0100

    version proto

--- supralternance/src/main/java/com/example/supralternance/SupralternanceApplication.java
+++ supralternance/src/main/java/com/example/supralternance/SupralternanceApplication.java
@@ -1,10 +1,13 @@
 package com.example.supralternance;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@Configuration
 public class SupralternanceApplication {
 
 	
